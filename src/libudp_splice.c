ae15539e4971a1f7c9e2d55c27759391fbdaa896
Changli Gao <xiaosuo@gmail.com>
2014-07-06 23:36:24 +0800

    Update the copyright notice

--- src/libudp_splice.c
+++ src/libudp_splice.c
@@ -1,6 +1,6 @@
 /**
  * udp_splice - Splice two UDP sockets.
- * Copyright (C) 2013 Changli Gao <xiaosuo@gmail.com>
+ * Copyright (C) 2013-2014 Changli Gao <xiaosuo@gmail.com>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
